57f611eb2ca488a354b8968b47ab6239e91b9a38
Marcin <mar.turkiewicz@gmail.com>
2025-12-15 00:18:29 +0100

    Refactor repo management and update commands
    Consolidate repo enabling and update commands for consistency.

--- disable-repo-and-update.sh
+++ disable-repo-and-update.sh
@@ -2,9 +2,6 @@
 set -euo pipefail
 . /etc/os-release
 
-# Disable ALL repos first
-subscription-manager repos --disable="*" || true
-
 # Packages you want to update
 PACKAGES=(
   'kernel*'
@@ -15,27 +12,22 @@ PACKAGES=(
   'ipa*'
 )
 
-# Update OS
+# Disable ALL repos first
+subscription-manager repos --disable="*" || true
 
+# Update OS
 case "$PLATFORM_ID" in
   *:el10*)
       echo "el10"
-      subscription-manager repos --enable=REPO1
-      subscription-manager repos --enable=REPO2
-      yum update "${PACKAGES[@]}" || true
-      ;;
+      subscription-manager repos --enable=REPO1 --enable=REPO2; yum update -y "${PACKAGES[@]}" || true
       ;;
   *:el9*)
       echo "el9"
-      subscription-manager repos --enable=REPO1
-      subscription-manager repos --enable=REPO2
-      yum update "${PACKAGES[@]}" || true
+      subscription-manager repos --enable=REPO1 --enable=REPO2; yum update -y "${PACKAGES[@]}" || true
       ;;
   *:el8*)
       echo "el8"
-      subscription-manager repos --enable=REPO1
-      subscription-manager repos --enable=REPO2
-      yum reinstall "${PACKAGES[@]}" || true
+      subscription-manager repos --enable=REPO1 --enable=REPO2; yum update -y "${PACKAGES[@]}" || true
       ;;
   *)
       echo "unknown: $PLATFORM_ID" >&2
